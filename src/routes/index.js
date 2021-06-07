db626e24123e769657b64e677eea297f91edd84a
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-06-07 03:53:23 -0300

    The /register path is added

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route, Switch } from "wouter";
 import { HomePage } from "pages/Home";
 import { SignInPage } from "pages/SignIn";
+import { SignUpPage } from "pages/SignUp";
 
 export function Routes() {
   return (
     <Switch>
       <Route path="/" component={HomePage} exact />
       <Route path="/login" component={SignInPage} exact />
+      <Route path="/register" component={SignUpPage} exact />
       <Route>404, Not Found!</Route>
     </Switch>
   );
