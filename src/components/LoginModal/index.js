8e96af7c6969263ba9147596d952e429e9bd3822
Cáceres Rodolfo <caceresrodolfo28@gmail.com>
2021-06-26 01:48:48 -0300

    The login feature is added from the modal

--- src/components/LoginModal/index.js
+++ src/components/LoginModal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
     Button,
     Modal,
@@ -10,11 +10,69 @@ import {
     ModalCloseButton,
     FormControl,
     FormLabel,
-    Input
+    Input,
+    InputGroup,
+    InputRightElement,
+    FormErrorMessage,
 } from "@chakra-ui/react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { useForm } from "react-hook-form";
+import { validateEmail, validatePassword } from "config/validations";
+import { useLocation } from "wouter";
+import { useToast } from "@chakra-ui/react";
+import { useSelector, useDispatch } from "react-redux";
+import { loginAction, userSelector, clearState } from "reducers/userSlice";
+
 import { colors } from "config/colorPalette";
 
+let showToast = true;
+
 export function LoginModal({ isOpen, onClose }) {
+
+    const [show, setShow] = useState(false);
+    const handleClick = () => setShow(!show);
+
+    const dispatch = useDispatch();
+    const toast = useToast();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const { isFetching, isSuccess, isError, errorMessage } = useSelector(userSelector);
+
+    const onSubmit = data => {
+        dispatch(loginAction(data));
+        showToast = true;
+    };
+
+    useEffect(() => {
+        return () => {
+            dispatch(clearState());
+        };
+    }, [dispatch]);
+
+    useEffect(() => {
+        if (isError) {
+            if (showToast) {
+                toast({
+                    title: "Error",
+                    description: errorMessage,
+                    status: "error",
+                    duration: 3000,
+                    isClosable: true,
+                });
+                showToast = !showToast;
+                dispatch(clearState());
+            }
+        }
+
+        if (isSuccess) {
+            onClose();
+        }
+    }, // eslint-disable-next-line 
+        [isError, isSuccess]);
 
     return (
         <>
@@ -23,33 +81,64 @@ export function LoginModal({ isOpen, onClose }) {
                 onClose={onClose}
             >
                 <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>Accedé a tu cuenta</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody pb={6}>
-                        <FormControl>
-                            <FormLabel>Ingresá tu email</FormLabel>
-                            <Input placeholder="Email" />
-                        </FormControl>
+                <form>
 
-                        <FormControl mt={4}>
-                            <FormLabel>Ingresá tu contraseña</FormLabel>
-                            <Input placeholder="Contraseña" />
-                        </FormControl>
-                    </ModalBody>
+                    <ModalContent>
+                        <ModalHeader>Accedé a tu cuenta</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody pb={6}>
 
-                    <ModalFooter>
-                        <Button
-                            w={"80%"}
-                            m="auto"
-                            bg={colors.orange1}
-                            //isLoading={true}
-                            loadingText="Enviando"
-                        >
-                            Iniciar sesión
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
+                            <FormControl m={2} isInvalid={errors.email}>
+                                <FormLabel>Ingresá tu email</FormLabel>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    placeholder="Email"
+                                    {...register("email", validateEmail)}
+                                />
+                                <FormErrorMessage>
+                                    {errors.email && errors.email.message}
+                                </FormErrorMessage>
+                            </FormControl>
+
+                            <FormControl m={2} isInvalid={errors.password}>
+                                <FormLabel>Ingresá tu contraseña</FormLabel>
+                                <InputGroup>
+                                    <Input
+                                        id="password"
+                                        type={show ? "text" : "password"}
+                                        placeholder="Contraseña"
+                                        {...register("password", validatePassword)}
+                                    />
+                                    <InputRightElement width="4.5rem">
+                                        <Button h="1.75rem" size="sm" onClick={handleClick}>
+                                            {show ? <ViewOffIcon /> : <ViewIcon />}
+                                        </Button>
+                                    </InputRightElement>
+                                </InputGroup>
+                                <FormErrorMessage>
+                                    {errors.password && errors.password.message}
+                                </FormErrorMessage>
+                            </FormControl>
+
+                        </ModalBody>
+
+                        <ModalFooter>
+                            <Button
+                                onClick={handleSubmit(onSubmit)}
+                                bg={colors.orange1}
+                                type="submit"
+                                isLoading={isFetching}
+                                loadingText="Enviando"
+                                w="80%"
+                                m="auto"
+                            >
+                                Iniciar sesión
+                            </Button>
+                        </ModalFooter>
+                    </ModalContent>
+
+                </form>
             </Modal>
         </>
     )
---
+++ src/config/validations.js
@@ -0,0 +1,13 @@
+export const validateEmail = {
+    required: "Debes ingresar tu email",
+    pattern: {
+        value:
+            // eslint-disable-next-line
+            /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/,
+        message: "El email es invalido",
+    },
+};
+
+export const validatePassword = {
+    required: "Debes ingresar tu contraseña",
+};
