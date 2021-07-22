5e2ffd675116ab67336b87426459fa1404b1f7f8
Rodolfo Caceres <caceresvictorrodolfo@gmail.com>
2021-07-21 21:03:44 -0300

    The header of the contact form is added

---
+++ src/pages/Contacts/components/ContactHeader.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
+
+export function ContactHeader() {
+  return (
+    <Box textAlign="center">
+      <Heading>Contactanos</Heading>
+    </Box>
+  );
+}
