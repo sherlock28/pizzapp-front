70fb7506e918d33244d1be98d48714308c44ef79
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2021-08-07 18:15:11 -0300

    Authentication path fix

--- src/services/settings.js
+++ src/services/settings.js
@@ -4,8 +4,8 @@ export const API_RESOURCES = {
     products: `${API_URL}/products`,
     orders: `${API_URL}/orders`,
     auth: {
-        signin: `${API_URL}/auth/signin`,
-        signup: `${API_URL}/auth/signup`,
-        signout: `${API_URL}/auth/signout`,
+        signin: `${API_URL}/signin`,
+        signup: `${API_URL}/signup`,
+        signout: `${API_URL}/signout`,
     },
 }
