de463236996430d16f48a323205448f516d8253c
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2021-08-18 23:52:48 -0300

    Fixed warninngs

--- src/pages/MyAccount/index.js
+++ src/pages/MyAccount/index.js
@@ -14,6 +14,15 @@ export function MyAccountPage() {
     password: "************"
   }
 
+  const handleEditPass = () => {
+    console.log("Edit Password");
+  }
+
+  const handleEditPhone = () => {
+    console.log("Edit Phone");
+  }
+
+
   return (
     <>
       <Heading fontWeight="bold" as="h2" size="lg">
@@ -23,8 +32,8 @@ export function MyAccountPage() {
         <Flex p={2} mb={4}>
           <Avatar size="lg" src=""/>
           <Flex flexDir="column" mx={2}>
-            <Text fontSize={"1.5rem"} fontWeight="light" fontWeight={500}>&nbsp;&nbsp;{user.name}</Text>
-            <Text fontSize={"1rem"} fontWeight="light" lineHeight={1}>{user.email}</Text>
+            <Text fontSize={"1.5rem"} fontWeight={500}>&nbsp;&nbsp;{user.name}</Text>
+            <Text fontSize={"1rem"} fontWeight="light" lineHeight={1}>&nbsp;&nbsp;{user.email}</Text>
           </Flex>
         </Flex>
         <Divider orientation="horizontal" />
@@ -32,12 +41,12 @@ export function MyAccountPage() {
           <Heading as="h5" size="sm" my="1rem">Usuario y clave</Heading>
           <Box mb="0.8rem">
             <Heading as="h6" size="xs" fontWeight="light" mb="0.5rem">Nombre de usuario</Heading>
-            <Text fontSize={"1rem"} fontWeight="light" fontWeight={500}>{user.username}</Text>
+            <Text fontSize={"1rem"} fontWeight="light">{user.username}</Text>
           </Box>
           <Box>
             <Heading as="h6" size="xs" fontWeight="light" mb="0.5rem">Clave</Heading>
               <Text fontSize={"1rem"} fontWeight="light">{user.password}</Text>
-              <Button color={`${colors.orange1}`} variant="link" mx={2}>Editar</Button>
+              <Button color={`${colors.orange1}`} variant="link" mx={2} onClick={handleEditPass}>Editar</Button>
           </Box>
         </Flex>
         <Divider orientation="horizontal" />
@@ -45,12 +54,12 @@ export function MyAccountPage() {
           <Heading as="h5" size="sm" my="1rem">Datos personales</Heading>
           <Box mb="0.8rem">
             <Heading as="h6" size="xs" fontWeight="light" mb="0.5rem">Nombre</Heading>
-            <Text fontSize={"1rem"} fontWeight="light" fontWeight={500}>{user.name}</Text>
+            <Text fontSize={"1rem"} fontWeight="light">{user.name}</Text>
           </Box>
           <Box>
             <Heading as="h6" size="sm" fontWeight="light" mb="0.5rem">Teléfono</Heading>
               <Text fontSize={"1rem"} fontWeight="light">{user.phone}</Text> 
-              <Button color={`${colors.orange1}`} variant="link" mx={2}>Editar</Button>
+              <Button color={`${colors.orange1}`} variant="link" mx={2} onClick={handleEditPhone}>Editar</Button>
           </Box>
         </Flex>
       </Container>
